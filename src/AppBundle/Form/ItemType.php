752d5a7b93b4f2fa9ef0aa53b6c39fe9610a3f6c
josja_benard@hotmail.com <vagrant@countdown>
2017-09-05 17:20:28 +0000

    COUNTDOWNBUNDLEREFACTOR Finished mergin the countdown bundle into the appBundle.

--- src/AppBundle/Form/ItemType.php
+++ src/AppBundle/Form/ItemType.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Form;
 
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
