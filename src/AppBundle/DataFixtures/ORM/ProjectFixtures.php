3494a99874dc7c7aaf3fd0ee5271ce243b5e1096
vagrant <vagrant@countdown>
2017-09-16 18:53:52 +0000

    DATAFIXTURES Created fixture for users.

---
+++ src/AppBundle/DataFixtures/ORM/ProjectFixtures.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jos
+ * Date: 16-9-2017
+ * Time: 17:42
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+class ProjectFixtures
+{
+
+}
---
+++ src/AppBundle/DataFixtures/ORM/UserFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use FOS\UserBundle\Model\UserManager;
+
+class UserFixtures extends Fixture
+{
+
+    static $DATA = [
+        'Jos' => [
+            'username' => 'Jos',
+            'email' => 'josja_benard@hotmail.com',
+            'password' => 'apollo010',
+            'roles' => ['ROLE_ADMIN'],
+        ],
+        'user' => [
+            'username' => 'User',
+            'email' => 'user@test.com',
+            'password' => 'apollo010',
+            'roles' => ['ROLE_USER'],
+        ],
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        /** @var UserManager $userManager */
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        foreach ($this::$DATA as $userData) {
+            $user = $userManager->createUser();
+            $user->setUsername($userData['username'])
+                ->setEmail($userData['email'])
+                ->setPlainPassword('password');
+
+            foreach ($userData['roles'] as $role) : $user->addRole($role); endforeach;
+
+            $userManager->updateUser($user);
+        }
+    }
+}
